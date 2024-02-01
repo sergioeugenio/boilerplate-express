7bff89654a59c6fd17d3f4656d0489e5d3fe6fa9
Sergio Eugenio <sergio.eugenio@gmail.com>
2024-02-01 10:15:13 +0000

    Ejercicio 01 al 04

--- myApp.js
+++ myApp.js
@@ -1,39 +1,28 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+// Ejercicio 01
+/*
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
+*/
 
+// Ejercicio 02
+/*
+let absolutepath = __dirname + "/views/index.html";
+app.get("/", function (req, res) {
+  res.sendFile(absolutepath);
+});
+*/
 
+//Ejercicio 03
+/*
+//let publicpath = __dirname + "/public";
+app.use("/public", express.static(__dirname + "/public"));
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// Ejercicio 04
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
+module.exports = app;
