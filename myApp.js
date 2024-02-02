ea382ff9c4270d0c49ec5272846dde7ff61d1697
Sergio Eugenio <sergio.eugenio@gmail.com>
2024-02-02 16:54:43 +0000

    Challenge 5

--- myApp.js
+++ myApp.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 let express = require("express");
 let app = express();
-// Ejercicio 01
+// Challenge 01
 /*
 app.get("/", function (req, res) {
   res.send("Hello Express");
 });
 */
 
-// Ejercicio 02
+// Challenge 02
 /*
 let absolutepath = __dirname + "/views/index.html";
 app.get("/", function (req, res) {
@@ -16,20 +16,31 @@ app.get("/", function (req, res) {
 });
 */
 
-//Ejercicio 03
+//Challenge 03
 /*
 //let publicpath = __dirname + "/public";
 app.use("/public", express.static(__dirname + "/public"));
 */
 
-// Ejercicio 04
+// Challenge 04
 /*
 app.get("/json", function (req, res) {
     res.json({ message: "Hello json" });
   });
 */
 
-// Ejercicio 05
+// Challenge 05
+/*
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+*/
+
+// Challenge 06
 app.get("/json", function (req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({ message: "HELLO JSON" });
